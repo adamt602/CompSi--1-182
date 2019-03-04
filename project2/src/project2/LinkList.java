5d7671e1551ead23e0436fa83536785fd13878f5
adamt602 <47990155+adamt602@users.noreply.github.com>
2019-03-04 10:18:13 -0800

    the

---
+++ project2/src/project2/LinkList.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project2;
+
+/**
+ *
+ * @author Bioni
+ */
+public class LinkList {
+    
+    
+    public static void main(String[] args){
+        
+//        String name = "Hello World";
+//        String the = "Hello World";
+//        System.out.println(name.toString() == the.toString());
+
+String name = new String("the");
+String the = new String ("the");
+the.concat("!");
+System.out.println(the);       
+        
+    }
+    
+}
